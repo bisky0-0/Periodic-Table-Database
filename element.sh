82551b80c87cf79ca048a6d0a2e7f2d4a91cb832
Biscuit krueger <yasmeinelkenany@gmail.com>
2024-07-01 15:27:32 +0000

    feat: add selected element variable

--- element.sh
+++ element.sh
@@ -5,4 +5,7 @@ echo "Please provide an element as an argument."
 
 read ARG
 
+SELECTED_ELEMENT=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$ARG' OR name='$ARG' OR atomic_number=$ARG")
+echo $SELECTED_ELEMENT
+
 
